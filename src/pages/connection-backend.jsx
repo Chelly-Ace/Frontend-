b27679dc4ee0c5289dc37d71a4e9068d1d318bf7
Albert De Villa <devillalbert@gmail.com>
2026-01-07 21:56:17 +0800

    fixes front-end

--- src/pages/connection-backend.jsx
+++ src/pages/connection-backend.jsx
@@ -1,7 +1,7 @@
 
 import { useEffect, useState } from "react";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = import.meta.env.VITE_API_URL?.replace(/\/$/, "");
 
 
 console.log("API BASE URL:", API_BASE_URL);
